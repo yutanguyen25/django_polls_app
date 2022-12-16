fa2af4e6fa58b17622193c50428c44d18059dac1
Yuta <yuta.nguyen@techvify.com.vn>
2022-12-16 15:37:33 +0700

    Reformat file

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
-app_name = 'root'
+app_name = "root"
 urlpatterns = [
-    path('polls/', include('polls.urls'), name='polls_app'),
+    path("polls/", include("polls.urls"), name="polls_app"),
     # path('todo/', include('todo.urls'), name='todo_app'),
     # path('blog/', include('blog.urls'), name='blog_app'),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
