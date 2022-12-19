1fba24e3d73c9c81ec03871db48313f926d328a4
trieunguyen <yuta.nguyen25@gmail.com>
2022-12-19 23:54:54 +0700

    Feat: Update isort

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
+from django.views import generic
 
 from polls.models import Choice, Question
 
